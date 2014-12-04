9a80358312ae3108a475cf69ffa9fcb31c2f5b82
Saman Jahanbakhsh <samanj@kth.se>
2014-12-04 16:00:49 +0100

    fixed som issue

--- build/generated-sources/ap-source-output/superMarket/Item_.java
+++ build/generated-sources/ap-source-output/superMarket/Item_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import superMarket.Person;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-04T14:14:00")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2014-12-04T15:43:51")
 @StaticMetamodel(Item.class)
 public class Item_ { 
 
